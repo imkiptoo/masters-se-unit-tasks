558048658c655106fc4a63151a56e7f982a20f70
Isaac Kiptoo <imkiptoo@gmail.com>
2024-10-08 16:31:55 +0200

    added more tasks: 2 and 3 and also added a selector for the task number

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,63 @@
+use std::io;
+use colored::*;
+
 fn main() {
-    let array: Vec<i32> = vec![1, 1];
+    println!("Please Select a Task:\n\
+    1. Task I   - Can Divide Array\n\
+    2. Task II  - The Evil Nine\n\
+    3. Task III - Happy Numbers\n");
 
-    let equal_on_both_sides = can_divide_array(array);
+    let mut task = String::new();
 
-    if equal_on_both_sides {
+    io::stdin()
+        .read_line(&mut task)
+        .expect("Failed to read line");
+
+    // Parse the input as an integer
+    match task.trim().parse::<u32>() {
+        Ok(1) => {
+            println!("Task I : \"Can Divide Array\" Selected");
+            can_divide_array(); // Call your function
+        },
+        Ok(2) => {
+            println!("The Evil Nine task selected.");
+            the_evil_nine();
+        },
+        Ok(3) => {
+            println!("Happy Numbers task selected.");
+            // Add logic for Task III
+        },
+        _ => {
+            println!("{}", "Please input a valid number!".red());
+        },
+    }
+}
+
+fn can_divide_array() {
+    let mut divisible = false;
+
+    let array: Vec<i32> = vec![1, 1, 3];
+
+    let array_length = array.len();
+
+    // Ensure array has an even number of elements
+    if array_length % 2 != 0 {
+        divisible = false
+    } else {
+        // Split the array into left and right sides
+        let (left_side, right_side) = array.split_at(array_length / 2);
+
+        // Sum both sides and compare
+        divisible = left_side.iter().sum::<i32>() == right_side.iter().sum::<i32>()
+    }
+
+    if divisible {
         println!("array is equal on both sides");
     } else {
         println!("array is not equal on both sides");
     }
 }
 
-fn can_divide_array(array: Vec<i32>) -> bool {
-    if array.len() % 2 != 0 {
-        return false;
-    }
+fn the_evil_nine(){
 
-    // split the array into left and right side
-    let array_length = array.len();
-    let left_side = &array[array_length / 2..];
-    let right_side = &array[..array_length / 2];
-
-    let mut left_side_sum = 0;
-    let mut right_side_sum = 0;
-
-    // sum left and right side
-    for num in left_side.iter() {
-        left_side_sum = left_side_sum + num;
-    }
-
-    for num in right_side.iter() {
-        right_side_sum = right_side_sum + num;
-    }
-
-    // check that left and right side are equal
-    if left_side_sum == right_side_sum {
-        return true;
-    }
-
-
-    return false;
 }
